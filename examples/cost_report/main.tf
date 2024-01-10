3c7f752c4333c6c88c6e4cf809990a64340e0ae4
Andy Cheung <andy@vantage.sh>
2024-01-09 19:38:26 -0500

    feat: Teams resource.

--- examples/cost_report/main.tf
+++ examples/cost_report/main.tf
@@ -6,29 +6,35 @@ terraform {
   }
 }
 
-resource "vantage_folder" "demo_folder" {
-  title = "Demo Folder"
-}
+# resource "vantage_folder" "demo_folder" {
+#   title = "Demo Folder"
+# }
 
-resource "vantage_folder" "demo_folder_child" {
-  title               = "Demo Folder First Child"
-  parent_folder_token = vantage_folder.demo_folder.token
-}
+# resource "vantage_folder" "demo_folder_child" {
+#   title               = "Demo Folder First Child"
+#   parent_folder_token = vantage_folder.demo_folder.token
+# }
 
-resource "vantage_saved_filter" "demo_filter" {
-  title  = "Demo Saved Filter"
-  filter = "(costs.provider = 'aws')"
-}
+# resource "vantage_saved_filter" "demo_filter" {
+#   title  = "Demo Saved Filter"
+#   filter = "(costs.provider = 'aws')"
+# }
 
-resource "vantage_cost_report" "demo_report" {
-  folder_token        = vantage_folder.demo_folder.token
-  filter              = "costs.provider = 'kubernetes'"
-  saved_filter_tokens = [vantage_saved_filter.demo_filter.token]
-  title               = "Demo Report"
-}
+# resource "vantage_cost_report" "demo_report" {
+#   folder_token        = vantage_folder.demo_folder.token
+#   filter              = "costs.provider = 'kubernetes'"
+#   saved_filter_tokens = [vantage_saved_filter.demo_filter.token]
+#   title               = "Demo Report"
+# }
 
-resource "vantage_dashboard" "demo_dashboard" {
-  widget_tokens = [vantage_cost_report.demo_report.token]
-  title         = "Demo Dashboard"
-  date_interval = "last_month"
+# resource "vantage_dashboard" "demo_dashboard" {
+#   widget_tokens = [vantage_cost_report.demo_report.token]
+#   title         = "Demo Dashboard"
+#   date_interval = "last_month"
+# }
+
+resource "vantage_team" "demo_team" {
+  name = "Demo Team"
+  description = "Demo Team Description"
+  user_emails = ["andy@vantage.sh"]
 }
