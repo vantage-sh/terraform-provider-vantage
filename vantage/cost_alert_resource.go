d5d752856a8bcdae8d42809b814786c30b690bc9
Tyler Perin <perintyler@gmail.com>
2025-04-24 10:33:41 -0400

    feat: Cost alerts TF support.

--- vantage/cost_alert_resource.go
+++ vantage/cost_alert_resource.go
@@ -3,21 +3,31 @@ package vantage
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/vantage-sh/terraform-provider-vantage/vantage/resource_cost_alert"
+	costalertsv2 "github.com/vantage-sh/vantage-go/vantagev2/vantage/cost_alerts"
 )
 
-var _ resource.Resource = (*costAlertResource)(nil)
+var (
+	_ resource.Resource                = (*costAlertResource)(nil)
+	_ resource.ResourceWithConfigure   = (*costAlertResource)(nil)
+	_ resource.ResourceWithImportState = (*costAlertResource)(nil)
+)
+
+type costAlertResource struct {
+	client *Client
+}
 
 func NewCostAlertResource() resource.Resource {
 	return &costAlertResource{}
 }
 
-type costAlertResource struct{}
-
-type costAlertResourceModel struct {
-	Id types.String `tfsdk:"id"`
+func (r *costAlertResource) Configure(_ context.Context, req resource.ConfigureRequest, _ *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+	r.client = req.ProviderData.(*Client)
 }
 
 func (r *costAlertResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -25,75 +35,99 @@ func (r *costAlertResource) Metadata(ctx context.Context, req resource.MetadataR
 }
 
 func (r *costAlertResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				Computed: true,
-			},
-		},
-	}
+	resp.Schema = resource_cost_alert.CostAlertResourceSchema(ctx)
 }
 
 func (r *costAlertResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data costAlertResourceModel
-
-	// Read Terraform plan data into the model
+	var data *costAlertModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Create API call logic
+	input := data.toCreate(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-	// Example data value setting
-	data.Id = types.StringValue("example-id")
+	params := costalertsv2.NewCreateCostAlertParams().WithCreateCostAlert(input)
+	out, err := r.client.V2.CostAlerts.CreateCostAlert(params, r.client.Auth)
+	if err != nil {
+		handleError("Create Cost Alert", &resp.Diagnostics, err)
+		return
+	}
 
-	// Save data into Terraform state
+	diag := data.applyPayload(ctx, out.Payload)
+	resp.Diagnostics.Append(diag...)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *costAlertResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data costAlertResourceModel
-
-	// Read Terraform prior state data into the model
+	var data *costAlertModel
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Read API call logic
+	params := costalertsv2.NewGetCostAlertParams().WithCostAlertToken(data.Token.ValueString())
+	out, err := r.client.V2.CostAlerts.GetCostAlert(params, r.client.Auth)
+	if err != nil {
+		if _, ok := err.(*costalertsv2.GetCostAlertNotFound); ok {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+		handleError("Read Cost Alert", &resp.Diagnostics, err)
+		return
+	}
 
-	// Save updated data into Terraform state
+	diag := data.applyPayload(ctx, out.Payload)
+	resp.Diagnostics.Append(diag...)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *costAlertResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data costAlertResourceModel
-
-	// Read Terraform plan data into the model
+	var data *costAlertModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Update API call logic
+	input := data.toUpdate(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-	// Save updated data into Terraform state
+	params := costalertsv2.NewUpdateCostAlertParams().
+		WithCostAlertToken(data.Token.ValueString()).
+		WithUpdateCostAlert(input)
+
+	out, err := r.client.V2.CostAlerts.UpdateCostAlert(params, r.client.Auth)
+	if err != nil {
+		handleError("Update Cost Alert", &resp.Diagnostics, err)
+		return
+	}
+
+	diag := data.applyPayload(ctx, out.Payload)
+	resp.Diagnostics.Append(diag...)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *costAlertResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data costAlertResourceModel
-
-	// Read Terraform prior state data into the model
+	var data *costAlertModel
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Delete API call logic
+	params := costalertsv2.NewDeleteCostAlertParams().
+		WithCostAlertToken(data.Token.ValueString())
+
+	_, err := r.client.V2.CostAlerts.DeleteCostAlert(params, r.client.Auth)
+	if err != nil {
+		handleError("Delete Cost Alert", &resp.Diagnostics, err)
+	}
+}
+
+func (r *costAlertResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("token"), req, resp)
 }
---
+++ vantage/cost_alert_resource_model.go
@@ -0,0 +1,98 @@
+package vantage
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/vantage-sh/terraform-provider-vantage/vantage/resource_cost_alert"
+	modelsv2 "github.com/vantage-sh/vantage-go/vantagev2/models"
+)
+
+type costAlertModel resource_cost_alert.CostAlertModel
+
+func (m *costAlertModel) applyPayload(ctx context.Context, payload *modelsv2.CostAlert) diag.Diagnostics {
+	var diag diag.Diagnostics
+	var list types.List
+
+	m.Token = types.StringValue(payload.Token)
+	m.Title = types.StringValue(payload.Title)
+	m.Interval = types.StringValue(payload.Interval)
+	m.UnitType = types.StringValue(payload.UnitType)
+
+	thresholdFloat, err := strconv.ParseFloat(payload.Threshold, 64)
+	if err != nil {
+		diag.AddError("Invalid Threshold", "Failed to parse threshold value: "+err.Error())
+		return diag
+	}
+	m.Threshold = types.Float64Value(thresholdFloat)
+
+	list, diag = types.ListValueFrom(ctx, types.StringType, payload.EmailRecipients)
+	if diag.HasError() {
+		return diag
+	}
+	m.EmailRecipients = list
+
+	list, diag = types.ListValueFrom(ctx, types.StringType, payload.SlackChannels)
+	if diag.HasError() {
+		return diag
+	}
+	m.SlackChannels = list
+
+	list, diag = types.ListValueFrom(ctx, types.StringType, payload.TeamsChannels)
+	if diag.HasError() {
+		return diag
+	}
+	m.TeamsChannels = list
+
+	list, diag = types.ListValueFrom(ctx, types.StringType, payload.ReportTokens)
+	if diag.HasError() {
+		return diag
+	}
+	m.ReportTokens = list
+
+	return diag
+}
+
+func (m *costAlertModel) toCreate(ctx context.Context, diags *diag.Diagnostics) *modelsv2.CreateCostAlert {
+	var threshold *float32
+	if !m.Threshold.IsNull() && !m.Threshold.IsUnknown() {
+		parsedFloat := float32(m.Threshold.ValueFloat64())
+		threshold = &parsedFloat
+	}
+
+	return &modelsv2.CreateCostAlert{
+		Title:           m.Title.ValueStringPointer(),
+		Interval:        m.Interval.ValueStringPointer(),
+		Threshold:       threshold,
+		UnitType:        m.UnitType.ValueStringPointer(),
+		EmailRecipients: terraformListToStrings(ctx, m.EmailRecipients, diags),
+		SlackChannels:   terraformListToStrings(ctx, m.SlackChannels, diags),
+		TeamsChannels:   terraformListToStrings(ctx, m.TeamsChannels, diags),
+		ReportTokens:    terraformListToStrings(ctx, m.ReportTokens, diags),
+	}
+}
+
+func (m *costAlertModel) toUpdate(ctx context.Context, diags *diag.Diagnostics) *modelsv2.UpdateCostAlert {
+	return &modelsv2.UpdateCostAlert{
+		Title:           m.Title.ValueString(),
+		Interval:        m.Interval.ValueString(),
+		Threshold:       float32(m.Threshold.ValueFloat64()),
+		UnitType:        m.UnitType.ValueString(),
+		EmailRecipients: terraformListToStrings(ctx, m.EmailRecipients, diags),
+		SlackChannels:   terraformListToStrings(ctx, m.SlackChannels, diags),
+		TeamsChannels:   terraformListToStrings(ctx, m.TeamsChannels, diags),
+		ReportTokens:    terraformListToStrings(ctx, m.ReportTokens, diags),
+	}
+}
+
+func terraformListToStrings(ctx context.Context, tfList types.List, diags *diag.Diagnostics) []string {
+	if tfList.IsNull() || tfList.IsUnknown() {
+		return nil
+	}
+	var out []string
+	diagnostics := tfList.ElementsAs(ctx, &out, false)
+	diags.Append(diagnostics...)
+	return out
+}
