04bd709f0d7d90de8c9e3ec7a4b9e3d918ffecb0
Andy Cheung <andy@vantage.sh>
2025-04-28 12:21:10 -0400

    acceptance test for anomaly notification and bugfix for update

---
+++ vantage/anomaly_notification_resource_test.go
@@ -0,0 +1,71 @@
+package vantage
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/vantage-sh/terraform-provider-vantage/vantage/acctest"
+)
+
+func TestAccAnomalyNotification_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAnomalyNotificationUsers() + testAccAnomalyNotificationCostReport() + testAccAnomalyNotification(10, "", "user_tokens = [data.vantage_users.users.users[0].token]"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("vantage_anomaly_notification.test", "token"),
+					resource.TestCheckResourceAttr("vantage_anomaly_notification.test", "threshold", "10"),
+				),
+			},
+			{ // update the threshold
+				Config: testAccAnomalyNotificationUsers() + testAccAnomalyNotificationCostReport() + testAccAnomalyNotification(20, "", "user_tokens = [data.vantage_users.users.users[0].token]"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vantage_anomaly_notification.test", "threshold", "20"),
+				),
+			},
+			{ // update the channels
+				Config: testAccAnomalyNotificationUsers() + testAccAnomalyNotificationCostReport() + testAccAnomalyNotification(20, "recipient_channels = [\"#test\"]", "user_tokens = [data.vantage_users.users.users[0].token]"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vantage_anomaly_notification.test", "threshold", "20"),
+				),
+			},
+		},
+	})
+}
+
+func testAccAnomalyNotificationUsers() string {
+	return `
+	data "vantage_users" "users" {}
+
+	`
+}
+
+func testAccAnomalyNotificationCostReport() string {
+	return `
+
+data "vantage_workspaces" "workspaces" {}
+
+resource "vantage_cost_report" "test" {
+	workspace_token = data.vantage_workspaces.workspaces.workspaces[0].token
+	title = "Test Cost Report"
+	filter = "costs.provider = 'aws'"
+	date_bin = "day"
+	chart_type = "line"
+}
+
+`
+}
+
+func testAccAnomalyNotification(threshold int, channelsStr, userTokensStr string) string {
+	return fmt.Sprintf(`
+resource "vantage_anomaly_notification" "test" {
+	cost_report_token = vantage_cost_report.test.token
+	threshold = %d
+	%s
+	%s
+}
+	`, threshold, channelsStr, userTokensStr)
+}
