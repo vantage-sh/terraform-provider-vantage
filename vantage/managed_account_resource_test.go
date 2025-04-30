f174a4349c39388e558e0f21f4bc58390d88c064
Andy Cheung <andy@vantage.sh>
2025-04-30 13:23:26 -0400

    test switching out the billing rules

--- vantage/managed_account_resource_test.go
+++ vantage/managed_account_resource_test.go
@@ -14,26 +14,51 @@ func TestAccManagedAccount_basic(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccBillingRule_adjustment("test", "service", "category", 50) + testAccManagedAccountResource(),
+				Config: testAccManagedAccountBillingRules() + testAccManagedAccountResource("br-1"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("vantage_managed_account.test", "name", "Test Account"),
 					resource.TestCheckResourceAttr("vantage_managed_account.test", "contact_email", "test@vantage.sh"),
+					resource.TestCheckResourceAttr("vantage_managed_account.test", "billing_rule_tokens.#", "1"),
 				),
 			},
-			// { // update the resource
-			// 	Config: testAccManagedAccountResource(),
-			// }
+			{
+				Config: testAccManagedAccountBillingRules() + testAccManagedAccountResource("br-2"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vantage_managed_account.test", "name", "Test Account"),
+					resource.TestCheckResourceAttr("vantage_managed_account.test", "contact_email", "test@vantage.sh"),
+					resource.TestCheckResourceAttr("vantage_managed_account.test", "billing_rule_tokens.#", "1"),
+				),
+			},
 		},
 	})
 }
+func testAccManagedAccountBillingRules() string {
+	return `
+resource "vantage_billing_rule" "br-1" {
+	title = "br1"
+	type = "adjustment"
+	service = "service"
+	category = "category"
+	percentage = 50.0
+}
 
-func testAccManagedAccountResource() string {
+resource "vantage_billing_rule" "br-2" {
+	title = "br1"
+	type = "adjustment"
+	service = "service"
+	category = "category"
+	percentage = 50.0
+}
+
+`
+}
+func testAccManagedAccountResource(billing_rule_id string) string {
 	return fmt.Sprintf(`
 
 resource "vantage_managed_account" "test" {
 	name                   = "Test Account"
 	contact_email           = "test@vantage.sh"
-	billing_rule_tokens = [vantage_billing_rule.test_adjustment.token]
+	billing_rule_tokens = [vantage_billing_rule.%[1]s.token]
 }
-	`)
+	`, billing_rule_id)
 }
