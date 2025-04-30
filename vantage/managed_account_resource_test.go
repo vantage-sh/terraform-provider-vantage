f77f0cbc06c5a4c750c6192f7b86dc8bc70d9eec
Andy Cheung <andy@vantage.sh>
2025-04-30 14:22:44 -0400

    attempt to read in env var for managed account domain name.

--- vantage/managed_account_resource_test.go
+++ vantage/managed_account_resource_test.go
@@ -2,6 +2,7 @@ package vantage
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -9,6 +10,7 @@ import (
 )
 
 func TestAccManagedAccount_basic(t *testing.T) {
+	t.Logf("Managed Account Domain name: %s", os.Getenv("MANAGED_ACCOUNT_DOMAIN"))
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -53,12 +55,17 @@ resource "vantage_billing_rule" "br-2" {
 `
 }
 func testAccManagedAccountResource(billing_rule_id string) string {
+	domain := os.Getenv("MANAGED_ACCOUNT_DOMAIN")
+	if domain == "" {
+		domain = "vantage.sh"
+	}
+
 	return fmt.Sprintf(`
 
 resource "vantage_managed_account" "test" {
 	name                   = "Test Account"
-	contact_email           = "test@vantage.sh"
-	billing_rule_tokens = [vantage_billing_rule.%[1]s.token]
+	contact_email           = "test@%[1]s"
+	billing_rule_tokens = [vantage_billing_rule.%[2]s.token]
 }
-	`, billing_rule_id)
+	`, domain, billing_rule_id)
 }
