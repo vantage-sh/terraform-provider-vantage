58ae196af9a07803ef6ea046f0db85209110a857
Conner Peirce <conner@vantage.sh>
2025-05-01 15:41:10 -0700

    test stub

---
+++ vantage/saved_filter_resource_test.go
@@ -0,0 +1,19 @@
+package vantage
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/vantage-sh/terraform-provider-vantage/vantage/acctest"
+)
+
+func TestAccVantageSavedFilter_basic(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps:                    []resource.TestStep{},
+		// ^ { Config:, Check:}, ...
+
+	})
+}
