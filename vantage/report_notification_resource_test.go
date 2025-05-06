8cb78642a577aec9597e66dbdd7241fa874a8010
Conner Peirce <conner@vantage.sh>
2025-05-06 15:48:48 -0700

    fix test.

--- vantage/report_notification_resource_test.go
+++ vantage/report_notification_resource_test.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"testing"
 
+	sdkacctest "github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/vantage-sh/terraform-provider-vantage/vantage/acctest"
 )
 
 func TestAccVantageReportNotification_basic(t *testing.T) {
+	costReportTitle := fmt.Sprint("cost report for notification ", sdkacctest.RandStringFromCharSet(10, sdkacctest.CharSetAlphaNum))
+	costReportFilter := "costs.provider = 'aws'"
 
 	id1 := "test-notification-1"
 	resourceName1 := "vantage_report_notification.test-notification-1"
@@ -21,8 +24,15 @@ func TestAccVantageReportNotification_basic(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccVantageReportNotification_basicTf(id1, title1, change1, frequency1),
+				Config: testAccVantageReportNotification_CreateTeamTf("team-1") +
+					costReportTF(costReportTitle, costReportFilter) +
+					testAccVantageReportNotification_basicTf(id1, title1, change1, frequency1, costReportTitle),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vantage_team.team", "name", "team-1"),
+
+					resource.TestCheckResourceAttr("vantage_cost_report.test", "title", costReportTitle),
+					resource.TestCheckResourceAttrSet("vantage_cost_report.test", "token"),
+
 					resource.TestCheckResourceAttr(resourceName1, "title", title1),
 					resource.TestCheckResourceAttr(resourceName1, "change", change1),
 					resource.TestCheckResourceAttr(resourceName1, "frequency", frequency1),
@@ -35,21 +45,29 @@ func TestAccVantageReportNotification_basic(t *testing.T) {
 	})
 }
 
-func testAccVantageReportNotification_basicTf(id, title, change, frequency string) string {
+func testAccVantageReportNotification_basicTf(id, title, change, frequency, costReportTitle string) string {
 	return fmt.Sprintf(
 		`
-		data "vantage_users" "test" {}
-		data "vantage_cost_reports" "test" {}
-		data "vantage_workspaces" "test" {}
-
 		 resource "vantage_report_notification" %[1]q {
 		   title = %[2]q
-					change = %[3]q
-					frequency = %[4]q
-					cost_report_token = data.vantage_cost_reports.test.cost_reports[0].token
-					user_tokens = [data.vantage_users.test.users[0].token]
-					workspace_token = data.vantage_workspaces.test.workspaces[0].token
+			 change = %[3]q
+			 frequency = %[4]q
+		   cost_report_token = resource.vantage_cost_report.test.token
+			 user_tokens = [data.vantage_users.test.users[0].token]
+			 workspace_token = data.vantage_workspaces.test.workspaces[0].token
 		 }
-		`, id, title, change, frequency,
+		`, id, title, change, frequency, costReportTitle,
 	)
+}
+
+func testAccVantageReportNotification_CreateTeamTf(name string) string {
+	return fmt.Sprintf(`
+data "vantage_users" "test" {}
+resource "vantage_team" "team" {
+	workspace_tokens = [data.vantage_workspaces.test.workspaces[0].token]
+	user_tokens = [data.vantage_users.test.users[0].token]
+	name = %[1]q
+	description = ""
+}
+	`, name)
 }
