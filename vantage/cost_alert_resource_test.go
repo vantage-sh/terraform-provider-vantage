b049167e3b6e2318985c4e0e823de254dcd774be
Tyler Perin <perintyler@gmail.com>
2025-05-13 11:55:45 -0400

    test: More cost alert resource/datasource tests.

--- vantage/cost_alert_resource_test.go
+++ vantage/cost_alert_resource_test.go
@@ -17,6 +17,7 @@ func TestCostAlert(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+			// Step 1: Create initial cost alert
 			{
 				Config: testAccWorkspacesDatasource() + testAccCostReport() + testAccCostAlertConfig(rTitle),
 				Check: resource.ComposeTestCheckFunc(
@@ -25,12 +26,20 @@ func TestCostAlert(t *testing.T) {
 					resource.TestCheckResourceAttrSet("vantage_cost_alert.test", "token"),
 				),
 			},
+			// Step 2: Update cost alert
 			{
 				Config: testAccWorkspacesDatasource() + testAccCostReport() + testAccCostAlertConfig(rUpdatedTitle),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("vantage_cost_alert.test", "title", rUpdatedTitle),
 				),
 			},
+			// Step 3: Confirm no changes after apply
+			{
+				Config:             testAccWorkspacesDatasource() + testAccCostReport() + testAccCostAlertConfig(rUpdatedTitle),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: false,
+			},
+			// Step 4: Check data source includes alert
 			{
 				Config: testAccWorkspacesDatasource() + testAccCostReport() + testAccCostAlertConfig(rUpdatedTitle) + testAccCostAlertsDataSource(),
 				Check: resource.ComposeTestCheckFunc(
@@ -38,47 +47,54 @@ func TestCostAlert(t *testing.T) {
 					resource.TestCheckResourceAttr("data.vantage_cost_alerts.test", "cost_alerts.0.title", rUpdatedTitle),
 				),
 			},
+			// Step 5: Delete the created test cost alert
+			{
+				Config: testAccWorkspacesDatasource() + testAccCostReport(),
+			},
+			// Step 6: Delete alert
+			{
+				Config: testAccWorkspacesDatasource() + testAccCostReport() + testAccCostAlertsDataSource(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.vantage_cost_alerts.test", "cost_alerts.#", "0"),
+				),
+			},
 		},
 	})
+}
+
+func testAccWorkspacesDatasource() string {
+	return `
+data "vantage_workspaces" "test_workspace" {}
+`
+}
+
+func testAccCostReport() string {
+	return `
+resource "vantage_cost_report" "test_cost_report" {
+  workspace_token = data.vantage_workspaces.test_workspace.workspaces[0].token
+  title           = "Test Cost Report"
+  chart_type      = "line"
+  date_bin        = "day"
+  date_interval   = "last_month"
+}
+`
+}
+
+func testAccCostAlertConfig(title string) string {
+	return fmt.Sprintf(`
+resource "vantage_cost_alert" "test" {
+  workspace_token = data.vantage_workspaces.test_workspace.workspaces[0].token
+  title           = %[1]q
+  threshold       = 100
+  interval        = "day"
+  unit_type       = "percentage"
+  report_tokens   = [vantage_cost_report.test_cost_report.token]
+}
+`, title)
 }
 
 func testAccCostAlertsDataSource() string {
 	return `
 data "vantage_cost_alerts" "test" {}
 `
-}
-
-func testAccWorkspacesDatasource() string {
-	return `
-data "vantage_workspaces" "test_workspace" {}
-
-`
-}
-func testAccCostReport() string {
-	return `
-
-resource "vantage_cost_report" "test_cost_report" {
-	workspace_token = data.vantage_workspaces.test_workspace.workspaces[0].token
-	title = "Test Cost Report"
-	chart_type = "line"
-	date_bin = "day"
-	date_interval = "last_month"
-}
-	
-`
-}
-
-func testAccCostAlertConfig(title string) string {
-	return fmt.Sprintf(`
-
-resource "vantage_cost_alert" "test" {
-	workspace_token = data.vantage_workspaces.test_workspace.workspaces[0].token
-	title           = %[1]q
-	threshold       = 100
-	interval        = "day"
-	unit_type       = "percentage"
-	report_tokens   = [vantage_cost_report.test_cost_report.token]
-}
-
-`, title)
 }
