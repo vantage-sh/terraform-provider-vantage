33fd39d5085e5b106f9cfab56d88d08774131483
Tyler Perin <perintyler@gmail.com>
2025-07-29 17:35:26 -0400

    test: Updating a report with custom columns.

--- vantage/resource_report_resource_test.go
+++ vantage/resource_report_resource_test.go
@@ -2,6 +2,7 @@ package vantage
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -13,7 +14,8 @@ func TestAccResourceReport(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			{ // create resource report
+			{
+			    // create resource report
 				Config: testAccResourceReport("test", "resources.provider = 'aws'"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("vantage_resource_report.resource_report", "title", "test"),
@@ -24,6 +26,17 @@ func TestAccResourceReport(t *testing.T) {
 				Config: testAccResourceReport("test2", "resources.provider = 'aws'"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("vantage_resource_report.resource_report", "title", "test2"),
+				),
+			},
+			{
+				// update resource report with columns
+				Config: testAccResourceReportWithColumns("test3", "resources.provider = 'aws' and resources.type = 'aws_instance'", []string{"provider", "label", "region"}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("vantage_resource_report.resource_report", "title", "test3"),
+					resource.TestCheckResourceAttr("vantage_resource_report.resource_report", "columns.#", "3"),
+					resource.TestCheckResourceAttr("vantage_resource_report.resource_report", "columns.0", "provider"),
+					resource.TestCheckResourceAttr("vantage_resource_report.resource_report", "columns.1", "label"),
+					resource.TestCheckResourceAttr("vantage_resource_report.resource_report", "columns.2", "region"),
 				),
 			},
 		},
@@ -41,4 +54,19 @@ resource "vantage_resource_report" "resource_report" {
 	filter = %[2]q
 }
 `, title, filter)
+}
+
+func testAccResourceReportWithColumns(title, filter string, columns []string) string {
+	columnsStr := `["` + strings.Join(columns, `", "`) + `"]`
+	return fmt.Sprintf(`
+
+data "vantage_workspaces" "test" {}
+
+resource "vantage_resource_report" "resource_report" {
+	workspace_token = data.vantage_workspaces.test.workspaces[0].token
+  title = %[1]q
+	filter = %[2]q
+	columns = %[3]s
+}
+`, title, filter, columnsStr)
 }
