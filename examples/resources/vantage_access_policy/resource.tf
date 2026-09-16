b8f65973400eb1a6205ffc3ed45fc0a4ac930524
Nick Funnell <nick@vantage.sh>
2026-09-16 13:07:52 -0700

    fix: Example update.

--- examples/resources/vantage_access_policy/resource.tf
+++ examples/resources/vantage_access_policy/resource.tf
@@ -1,10 +1,10 @@
 resource "vantage_access_policy" "demo_access_policy" {
-  title       = "Engineering costs"
-  description = "Limits cost visibility to the Engineering team."
+  title       = "AWS East Only Costs"
+  description = "Limits cost visibility to just east coast AWS."
   team_tokens = [vantage_team.demo_team.token]
 
   policy = {
     api_version = "v1"
-    filter      = "(vantage.provider = 'aws')"
+    filter      = "(vantage.provider = 'aws' AND vantage.region = 'us-east-1')"
   }
 }
